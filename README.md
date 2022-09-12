75df2ae3b1af0ec51986537fff61932ee6ea1d06
zhangbc <zhangbocheng189@163.com>
2022-09-12 23:33:09 +0800

    [init] init design patterns project.

---
+++ README.md
@@ -0,0 +1,5 @@
+# 项目说明
+ 
+> 本项目为设计模式学习笔记
+>
+> 资料来自B站：https://www.bilibili.com/video/BV1M5411N7uh?spm_id_from=333.999.0.0&vd_source=616bf6a004def119a1b27e570687e7ea
---
+++ src/main/java/com/team/singleton/Main.java
@@ -0,0 +1,11 @@
+package com.team.singleton;
+
+/**
+ * design-patteerns
+ *
+ * @author zhangbc
+ * @version v1.0.0
+ * @date 9/12/22 11:42 PM
+ **/
+public class Main {
+}
